c88c7a690c10dab8d543ce0731468b45357ba001
Anal Abortion <nedamtikunu@gmail.com>
2023-04-19 11:48:51 +0200

    -housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "hr.rao.android.plugin"
-version = "1.0.14-kts"
+version = "1.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -30,43 +30,6 @@ tasks.withType<Test> {
     testLogging {
         showStandardStreams = true
     }
-
-//    testLogging.exceptionFormat = TestExceptionFormat.FULL
-
-
-//    testLogging {
-//        showStandardStreams = true
-//
-//        // set options for log level LIFECYCLE
-//        events = setOf(
-//            TestLogEvent.FAILED,
-//            TestLogEvent.PASSED,
-//            TestLogEvent.SKIPPED,
-//            TestLogEvent.STANDARD_OUT
-//        )
-//
-//        exceptionFormat = TestExceptionFormat.FULL
-//        showExceptions = true
-//        showCauses = true
-//        showStackTraces = true
-//
-//        // set options for log level DEBUG and INFO
-//        debug {
-//            events = setOf(
-//                TestLogEvent.STARTED,
-//                TestLogEvent.FAILED,
-//                TestLogEvent.PASSED,
-//                TestLogEvent.SKIPPED,
-//                TestLogEvent.STANDARD_ERROR,
-//                TestLogEvent.STANDARD_OUT
-//            )
-//
-//            exceptionFormat = TestExceptionFormat.FULL
-//        }
-//
-//        info.events = debug.events
-//        info.exceptionFormat = debug.exceptionForma
-//    }
 }
 
 dependencies {
